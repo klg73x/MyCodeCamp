ed9ecbfa87f4258d5f983646ed1627d9025a330f
Kevin Gagnon <keving@kultera.com>
2018-05-15 07:41:41 -0400

    This ensures all calls are forced to the Https side when coming in. Even if the user had http in the address section.

--- MyCodeCamp/Startup.cs
+++ MyCodeCamp/Startup.cs
@@ -6,6 +6,7 @@ using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -22,8 +23,12 @@ namespace MyCodeCamp
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
                 .AddEnvironmentVariables();
+
+            _env = env; //This allows us to save this config to test what environment we are in
             _config = builder.Build();
         }
+
+        private IHostingEnvironment _env;
 
         IConfigurationRoot _config { get; }
 
@@ -38,7 +43,17 @@ namespace MyCodeCamp
             services.AddAutoMapper();
 
             // Add framework services.
-            services.AddMvc()
+            services.AddMvc(opt =>
+            {
+                //This section is for SSL. Using filters here will be GLOBAL accross all calls to the Api
+                //This filter will redirect a standard Http call to the service to instead use the Https version
+                if (!_env.IsProduction())
+                {
+                    opt.SslPort = 44388; //For redirection we need to explicitly tell the dev box which port we want
+                                                      //In production it will know to use port 443 which is standard
+                }
+                opt.Filters.Add(new RequireHttpsAttribute());
+            })
                 .AddJsonOptions(opt =>
                 {
                     opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
